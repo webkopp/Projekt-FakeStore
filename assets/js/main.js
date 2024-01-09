cda1b7b2d830af13a5f785f5aa356a05a132d0c1
webkopp <nakopps@googlemail.com>
2024-01-09 17:05:38 +0100

    fakeShop-js-update

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,36 +8,77 @@
 //             .then(res=>res.json())
 //             .then(json=>console.log(json))
 
-fetch('https://fakestoreapi.com/products?sort=price')
-            .then(res=>res.json())
-            .then(json=>console.log(json))
+const apiAllProduct = "https://fakestoreapi.com/products"
+const apiImage = "https://fakestoreapi.com/products/image"
+const apiCategories = "https://fakestoreapi.com/products/categories"
+const apiPrice = "https://fakestoreapi.com/products/price"
+
+
+const getApiFunction = (apiLink) => {
+    fetch(apiLink)
+        .then((resp) => resp.json())
+        .then((data) => {
+            const unsereData = [...data]
+            console.log(unsereData)
+            unsereData.forEach((singelProduct) => {
+                showProduct(singelProduct)
+            })
+        })
+}
+
+let main = document.querySelector("main")
+
+
+const showProduct = (einProduct) => {
+    console.log(einProduct.title);
+    main.innerHTML += `
+    <div>
+    <img src ="${einProduct.image}" alt="${einProduct.title}">
+    <h3>${einProduct.title}</h3>
+    <p>${einProduct.price} €</p>
+
+    `
+}
+
+getApiFunction(apiAllProduct)
+getApiFunction(apiImage)
+getApiFunction(apiCategories)
+getApiFunction(apiPrice)
+
+
+
+// version1
+// fetch('https://fakestoreapi.com/products?sort=price')
+//             .then(res=>res.json())
+//             .then(json=>console.log(json))
 
     
-// console.log(fetch("https://fakestoreapi.com/products?sort=desc"))
+// // console.log(fetch("https://fakestoreapi.com/products?sort=desc"))
 
-function sortProductsPrice(order) {
-    fetch("https://fakestoreapi.com/products?sort=price")
-    .then(response => response.json())
-    .then(data => {
-        if (order === "price") {
-            data.sort((a, b) => a.price - b.price)
-        } else if (order === "price") {
-            data.sort((a, b) => b.price - a.price)
-        }
-        // produktliste leeren
-        productList.innerHTML = ""
+// function sortProductsPrice(order) {
+//     fetch("https://fakestoreapi.com/products?sort=price")
+//     .then(response => response.json())
+//     .then(data => {
+//         const producti = document.getElementById("producti")
+//         if (order === "price") {
+//             data.sort((a, b) => a.price - b.price)
+//         } else if (order === "price") {
+//             data.sort((a, b) => b.price - a.price)
+//         }
+//         // produktliste leeren
+//         productList.innerHTML = ""
 
-        data.forEach(product => {
-            const productDiv = document.createElement("div")
-            productDiv.classList.add("product")
-            productDiv.innerHTML = `
-            <img src ="${product.image}" alt="${product.title}">
-            <h3>${product.title}</h3>
-            <p>${product.price} €</p>
-            <button onclick = "addToCart(${product.id})">Preis-Granate</button>
-            `
-            productList.appendChild(productDiv)
-        })
-    })
-    .catch((error) => console.log("so bombige Preise findest du nicht wieder", error))
-}
+//         data.forEach(product => {
+//             const productDiv = document.createElement("div")
+//             productDiv.classList.add("product")
+//             productDiv.innerHTML = `
+//             <img src ="${product.image}" alt="${product.title}">
+//             <h3>${product.title}</h3>
+//             <p>${product.price} €</p>
+//             <button onclick = "addToCart(${product.id})">Preis-Granate</button>
+//             `
+//             productList.appendChild(productDiv)
+//         })
+//     })
+//     .catch((error) => console.log("so bombige Preise findest du nicht wieder", error))
+// }
