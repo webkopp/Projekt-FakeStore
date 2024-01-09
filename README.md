305251d03edacbd26e5415784fc465baca5df020
webkopp <nakopps@googlemail.com>
2024-01-09 14:14:31 +0100

    project-019-fakeStor-API

---
+++ README.md
@@ -0,0 +1,24 @@
+*** FakeStore-API ***
+
+Beschreibung
+Dieses JavaScript-Projekt wurde im Rahmen eines Coding-Bootcamps bei supercode.de erstellt. Es handelt sich um einen E-Commerce-Store, für die man die Daten einer Fake-Store-API verwendet und man im Shop die Produkte nach Preis sortieren kann.
+
+Die Anwendung bietet folgende Möglichkeiten:
+<!-- - Das Input-Feld bietet die Möglichkeit nach Klamotten zu suchen. -->
+- Über den Submit-Button "Preis-Granate" werden die Artikel mit dem besten Preis angezeigt.
+
+Werkzeuge/Technologien
+HTML5
+CSS3
+JavaScript
+
+GitHub Live-Vorschau
+Du findest eine Live Vorschau auf GitHub: 
+
+GitHub-Repository
+Du findest das gesamte Projekt auf GitHub: 
+
+Aufgaben
+- Erstelle einen kleinen E-Commerce Store mit der [FakeStore_API](https://fakestoreapi.com/).
+- **Überlege dir ein ansprechendes Design.**
+- Schaue dir die verschiedenen Endpoints genau an: https://fakestoreapi.com/docs.
---
+++ assets/js/main.js
@@ -0,0 +1,43 @@
+// - Erstelle einen kleinen E-Commerce Store mit der [FakeStore_API](https://fakestoreapi.com/).
+// - **Überlege dir ein ansprechendes Design.**
+// - Schaue dir die verschiedenen Endpoints genau an: https://fakestoreapi.com/docs
+
+// import { variables, table } from "./variables.js";
+
+// fetch('https://fakestoreapi.com/products')
+//             .then(res=>res.json())
+//             .then(json=>console.log(json))
+
+fetch('https://fakestoreapi.com/products?sort=desc')
+            .then(res=>res.json())
+            .then(json=>console.log(json))
+
+    
+// console.log(fetch("https://fakestoreapi.com/products?sort=desc"))
+
+function sortProductsPrice(order) {
+    fetch("https://fakestoreapi.com/products")
+    .then(response => response.json())
+    .then(data => {
+        if (order === "asc") {
+            data.sort((a, b) => a.price - b.price)
+        } else if (order === "desc") {
+            data.sort((a, b) => b.price - a.price)
+        }
+        // produktliste leeren
+        productList.innerHTML = ""
+
+        data.forEach(product => {
+            const productDiv = document.createElement("div")
+            productDiv.classList.add("product")
+            productDiv.innerHTML = `
+            <img src ="${product.image}" alt="${product.title}">
+            <h3>${product.title}</h3>
+            <p>${product.price} €</p>
+            <button onclick = "addToCart(${product.id})">Preis-Granate</button>
+            `
+            productList.appendChild(productDiv)
+        })
+    })
+    .catch((error) => console.log("so bombige Preise findest du nicht wieder", error))
+}
